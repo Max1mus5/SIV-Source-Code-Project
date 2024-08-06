84d6fffdf1d4266ef691be5f67c2037b8dfebd53
Jeroimo <jeronimoriveros@gmail.com>
2024-08-06 00:01:46 -0500

    fix(solve web3 error): fix Web3.providers.HttpProvider error
    Web3.providers.HttpProvider is not a constructor

--- Blog-Backend/connection/blockchain/etherum-blockchain/infura.js
+++ Blog-Backend/connection/blockchain/etherum-blockchain/infura.js
@@ -1,7 +1,7 @@
-var { Web3 } = require("web3");
-var provider = 'https://mainnet.infura.io/v3/afeb52f2996b471f878d0bf69d65ca66';
-var web3Provider = new Web3.providers.HttpProvider(provider);
-var web3 = new Web3(web3Provider);
+const { Web3 } = require("web3");
+const provider =process.env.INFURA_BLOCKCHAIN_URL;
+const web3Provider = new Web3.providers.HttpProvider(provider);
+const web3 = new Web3(web3Provider);
 
 web3.eth.getBlockNumber().then((result) => {
   console.log("Latest Ethereum Block is ", result);
