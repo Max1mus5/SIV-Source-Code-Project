2f84ad99cf6b9fac3473e5484c5417584d607d36
Jeroimo <jeronimoriveros@gmail.com>
2025-01-09 23:43:14 -0500

    updating return format for verify if the mail is verifyed

--- Blog-Backend/modules/user/controller/login.js
+++ Blog-Backend/modules/user/controller/login.js
@@ -1,30 +1,38 @@
-// Modules/users/Login.js
 const { User: UserSchema } = require('../../../connection/db/schemas/user-schema/userSchema'); 
 const { generateToken } = require('../../../connection/middlewares/JWTmiddleware');
 const bcrypt = require('bcryptjs');
-
 class LoginController {
     async login(req, res) {
         const { username, password } = req.body;
         const secretKey = process.env.JWT_SECRET;
 
         try {
-            const user = await UserSchema.findOne({ where: { name : username } });
+            const user = await UserSchema.findOne({ where: { name: username } });
 
             if (!user) {
-                return 'Invalid username';
+                return res.status(401).json({ 
+                    status: 'error', 
+                    message: 'Nombre de usuario inválido' 
+                }); 
             }
             
             const validPassword = await bcrypt.compareSync(password, user.password);
 
             if (!validPassword) {
-                return  'Invalid password';
+                return res.status(401).json({ 
+                    status: 'error', 
+                    message: 'Contraseña inválida' 
+                }); 
             }
 
             const token = generateToken({ id: user.id, username: user.username, role: user.role }, secretKey, 14400);
-            return token;
+            
+            return { token, user }; // Retorna un objeto con token y user
         } catch (error) {
-            return error.message;
+            return res.status(500).json({ 
+                status: 'error', 
+                message: 'Error en el servidor' 
+            }); 
         }
     }
 }
