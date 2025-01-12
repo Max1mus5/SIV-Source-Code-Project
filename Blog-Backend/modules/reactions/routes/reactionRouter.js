75a5b9070a68a77bf9c14b8e7021b3485b59afbf
Jeroimo <jeronimoriveros@gmail.com>
2025-01-11 23:21:02 -0500

    create and delete reactions function

--- Blog-Backend/modules/reactions/routes/reactionRouter.js
+++ Blog-Backend/modules/reactions/routes/reactionRouter.js
@@ -4,3 +4,22 @@ const ReactionController = require('../controller/reactionController');
 const { handleErrorResponse } = require('../utils/utils');
 const { authenticateToken } = require('../../../connection/middlewares/JWTmiddleware');
 
+router.post('/create', authenticateToken, async (req, res) => {
+  const reactionController = new ReactionController();
+  try {
+      const newReaction = await reactionController.createReaction(req.body);
+      res.status(201).json(newReaction);
+  } catch (error) {
+      handleErrorResponse(res, error);
+  }
+});
+
+router.delete('/delete/:reactionId', authenticateToken, async (req, res) => {
+  const reactionController = new ReactionController();
+  try {
+      const result = await reactionController.deleteReaction(req.params.reactionId, req.user.id);
+      res.status(200).json(result);
+  } catch (error) {
+      handleErrorResponse(res, error);
+  }
+});
